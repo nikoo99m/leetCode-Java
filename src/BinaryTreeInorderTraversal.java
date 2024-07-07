e174ae3e633e231f36ca953798cf82b7951e4724
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-07-07 22:18:28 +0100

    feat: Adding inorder traversal implementation for binary tree.

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+public class BinaryTreeInorderTraversal {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+}
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private static void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> resultRecursive = inorderTraversal(root);
+        System.out.println(resultRecursive);  // Output: [1, 3, 2]
+
+    }
+}
+
+
+
